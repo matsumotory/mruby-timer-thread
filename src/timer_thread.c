f675c3dfebca788dc93cd2a8daa4ca63fc172a01
Uchio KONDO <udzura@pepabo.com>
2017-03-16 18:47:50 +0900

    Support custom signal

--- src/timer_thread.c
+++ src/timer_thread.c
@@ -9,12 +9,16 @@
 #include <errno.h>
 #include <math.h>
 #include <signal.h>
+#include <signal.h>
 #include <time.h>
 
 #define DONE mrb_gc_arena_restore(mrb, 0);
 
+#define MRB_TIMER_POSIX_KEY_SIGNO mrb_intern_lit(mrb, "signal")
+
 typedef struct {
   timer_t *timer_ptr;
+  int timer_signo;
 } mrb_timer_posix_data;
 
 static void mrb_timer_posix_free(mrb_state *mrb, void *p)
@@ -31,6 +35,23 @@ static mrb_value mrb_timer_posix_init(mrb_state *mrb, mrb_value self)
 {
   mrb_timer_posix_data *data;
   timer_t *timer_ptr;
+  mrb_value options = mrb_nil_value();
+
+  struct sigevent sev;
+  sev.sigev_signo = 0;
+
+  if (mrb_get_args(mrb, "|o", &options) == -1) {
+    mrb_sys_fail(mrb, "mrb_get_args");
+  }
+
+  if (!mrb_nil_p(options)) {
+    mrb_value signo = mrb_hash_get(mrb, options, mrb_symbol_value(MRB_TIMER_POSIX_KEY_SIGNO));
+    if (mrb_fixnum_p(signo)) {
+      sev.sigev_notify = SIGEV_SIGNAL;
+      sev.sigev_signo = (int)mrb_fixnum(signo);
+    }
+  }
+
   data = (mrb_timer_posix_data *)DATA_PTR(self);
   if (data)
     mrb_timer_posix_free(mrb, data);
@@ -40,10 +61,18 @@ static mrb_value mrb_timer_posix_init(mrb_state *mrb, mrb_value self)
 
   data = (mrb_timer_posix_data *)mrb_malloc(mrb, sizeof(mrb_timer_posix_data));
   timer_ptr = (timer_t *)mrb_malloc(mrb, sizeof(timer_t));
-  if (timer_create(CLOCK_REALTIME, NULL, timer_ptr) == -1) {
-    mrb_sys_fail(mrb, "timer_create failed");
+
+  if (sev.sigev_signo == 0) {
+    if (timer_create(CLOCK_REALTIME, NULL, timer_ptr) == -1) {
+      mrb_sys_fail(mrb, "timer_create failed");
+    }
+  } else {
+    if (timer_create(CLOCK_REALTIME, &sev, timer_ptr) == -1) {
+      mrb_sys_fail(mrb, "timer_create failed");
+    }
   }
   data->timer_ptr = timer_ptr;
+  data->timer_signo = sev.sigev_signo;
 
   DATA_PTR(self) = data;
   return self;
@@ -135,9 +164,6 @@ static mrb_value mrb_timer_posix_stop(mrb_state *mrb, mrb_value self)
   return self;
 }
 
-#define MRB_TIMER_RES_SEC mrb_intern_lit(mrb, ":sec")
-#define MRB_TIMER_RES_MSEC mrb_intern_lit(mrb, ":msec")
-
 static mrb_value mrb_timer_posix_status_raw(mrb_state *mrb, mrb_value self)
 {
   mrb_timer_posix_data *data = DATA_PTR(self);
@@ -156,6 +182,12 @@ static mrb_value mrb_timer_posix_status_raw(mrb_state *mrb, mrb_value self)
   return ret;
 }
 
+static mrb_value mrb_timer_posix_signo(mrb_state *mrb, mrb_value self)
+{
+  mrb_timer_posix_data *data = DATA_PTR(self);
+  return mrb_fixnum_value(data->timer_signo);
+}
+
 void mrb_mruby_timer_gem_init(mrb_state *mrb)
 {
   struct RClass *timer, *posix;
@@ -163,11 +195,15 @@ void mrb_mruby_timer_gem_init(mrb_state *mrb)
 
   posix = mrb_define_class_under(mrb, timer, "POSIX", mrb->object_class);
   MRB_SET_INSTANCE_TT(posix, MRB_TT_DATA);
-  mrb_define_method(mrb, posix, "initialize", mrb_timer_posix_init, MRB_ARGS_NONE());
-  mrb_define_method(mrb, posix, "start", mrb_timer_posix_start, MRB_ARGS_ARG(1, 2));
-  mrb_define_method(mrb, posix, "start_hires", mrb_timer_posix_start_hires, MRB_ARGS_ARG(1, 2));
+  mrb_define_method(mrb, posix, "initialize", mrb_timer_posix_init, MRB_ARGS_ARG(0, 1));
+  mrb_define_method(mrb, posix, "start", mrb_timer_posix_start, MRB_ARGS_ARG(1, 1));
+  mrb_define_method(mrb, posix, "start_hires", mrb_timer_posix_start_hires, MRB_ARGS_ARG(1, 1));
   mrb_define_method(mrb, posix, "stop", mrb_timer_posix_stop, MRB_ARGS_NONE());
   mrb_define_method(mrb, posix, "__status_raw", mrb_timer_posix_status_raw, MRB_ARGS_NONE());
+
+  mrb_define_method(mrb, posix, "signo", mrb_timer_posix_signo, MRB_ARGS_NONE());
+
+  DONE;
 }
 
 void mrb_mruby_timer_gem_final(mrb_state *mrb)
