7d8ac985e421f3bac3fd79666924e67c965272f7
pyama86 <pyama@pepabo.com>
2017-03-18 17:19:54 +0900

    add test run_with_signal

--- test/mrb_timer_thread.rb
+++ test/mrb_timer_thread.rb
@@ -19,3 +19,22 @@ assert("Timer#run") do
 
   assert_true (start - finish) < (timer_msec / 1000 + 2)
 end
+
+assert("Timer#run_with_signal") do
+  timer_msec = 5000
+  finish = 1
+
+  SignalThread.trap(:USR1) do
+    finish = Time.now
+  end
+
+  timer = TimerThread.new
+  start = Time.now
+
+  timer.run_with_signal timer_msec, :USR
+
+  while timer.running? do
+    sleep 1
+  end
+  assert_true (start - finish) < (timer_msec / 1000 + 2)
+end
