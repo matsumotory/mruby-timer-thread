2ada4cfd7a8e85144c1f17447f655cae99c71d93
Uchio KONDO <udzura@pepabo.com>
2017-04-11 20:04:20 +0900

    Timeout too

--- test/posix_thread_test.rb
+++ test/posix_thread_test.rb
@@ -1,25 +1,35 @@
 assert("Timer::POSIX with RTSignal, interval timer and block") do
   timer_msec = 10
-  sem = false
+  timer_sem = 0
   v1 = 0
   v2 = 0
 
-  t1 = SignalThread.trap(:RT5) { v1 += 1; sem = true }
-  t2 = SignalThread.trap(:RT5) { v2 += 1; sem = true }
+  t2 = SignalThread.trap(:RT5) { v2 += 1; timer_sem = 1 }
+  t1 = SignalThread.trap(:RT5) { v1 += 1; timer_sem = 1 }
+  timeout = 0
 
   begin
     Timer::POSIX.new(thread_id: t2.thread_id, signal: :RT5).run(timer_msec)
-    until sem
-      usleep 1000 rescue nil
+    while timer_sem == 0
+      usleep 10 * 1000 rescue nil
+      timeout += 1
+      break if timeout >= 300
     end
     assert_equal 0, v1
     assert_equal 1, v2
-    sem = false
+    timer_sem = 0
+    timeout = 0
 
+    #t1.kill :RT5
     Timer::POSIX.new(thread_id: t1.thread_id, signal: :RT5).run(timer_msec)
-    until sem
+    while timer_sem == 0
       usleep 1000 rescue nil
+      timeout += 1
+      break if timeout >= 300
     end
+    p t1.exception
+    p t2.exception
+
     assert_equal 1, v1
     assert_equal 1, v2
   rescue NotImplementedError => e
